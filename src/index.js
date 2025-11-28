25dff0e4b64bfb7d9f0f670de4a625448b8153b4
Xavier Tondelier <xtondelier@gmail.com>
2025-11-28 13:44:48 +0000

    Add basic card class and dom displayer

--- src/index.js
+++ src/index.js
@@ -1,3 +1,43 @@
-import "./font-styles.css"
-import "./style.css"
-console.log("Hello World")
+import "./font-styles.css";
+import "./style.css";
+import Item from "./todo-item.js";
+
+const contentDiv = document.querySelector("div#content");
+
+const screenController = (function() {
+  function displayDomForItem(item) {
+    const card = document.createElement("div");
+    card.classList.add("card");
+
+    const title = document.createElement("h2");
+    title.textContent = item.title;
+    title.classList.add("title");
+
+    const description = document.createElement("p");
+    description.textContent = item.description;
+    description.classList.add("description");
+
+    const dueDate = document.createElement("p");
+    dueDate.textContent = item.dueDate;
+    dueDate.classList.add("due-date");
+
+    const priority = document.createElement("p");
+    priority.textContent = item.priority;
+    priority.classList.add("priority");
+
+    const notes = document.createElement("p");
+    notes.textContent = item.notes;
+    notes.classList.add("notes");
+
+    [title, description, dueDate, priority, notes].forEach(part => {
+      card.appendChild(part)
+    });
+
+    contentDiv.appendChild(card);
+  }
+
+  return { displayDomForItem }
+})();
+
+const item = new Item("Eating", "I need to eat", "12th March", "1", "I am just really hungry");
+screenController.displayDomForItem(item);
---
+++ src/todo-item.js
@@ -0,0 +1,9 @@
+export default class {
+  constructor(title, description, dueDate, priority, notes) {
+    this.title = title;
+    this.description = description;
+    this.dueDate = dueDate;
+    this.priority = priority;
+    this.notes = notes;
+  }
+}
